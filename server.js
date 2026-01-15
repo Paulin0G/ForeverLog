062780f6b669baf6bab1634bbb622704d88e50a4
Paulin0G <paulino0518@gmail.com>
2026-01-15 13:55:04 -0600

    Add / get route to render dashboard json messages

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 const port = 3000;
 
+app.get('/', (req, res) => {
+    try {
+        return res.status(200).json({ message: "Welcome to the homepage" });
+    } catch (error) {
+        return res.status(500).json({ message: "Error rendering dashboard" });
+        //Good to log this
+    }
+});
+
 app.listen(port, () => {
     console.log('Listening on port 3000');
 })
